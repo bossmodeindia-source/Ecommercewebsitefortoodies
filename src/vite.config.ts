2c6a96673eaacf1ff1e665398ecfc742de25801b
bossmodeindia-source <bossmodeindia@gmail.com>
2026-04-05 23:43:59 +0530

    Update vite.config.ts

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -36,8 +36,8 @@ export default defineConfig({
   },
 
   build: {
-    // !! Must match netlify.toml publish directory !!
-    outDir: 'dist',
+  // !! Must match netlify.toml publish directory !!
+  outDir: 'dist',
     emptyOutDir: true,
     sourcemap: false,
     minify: 'esbuild',
